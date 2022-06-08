5ab793dfc6367e672040374e0e4c80d8ab0f14b7
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 17:28:17 -0700

    Add README.md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *  main - Prints _putchar, followed by new line
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n = '_';
+	char p = 'p';
+	char u = 'u';
+	char t = 't';
+	char c = 'c';
+	char h = 'h';
+	char a = 'a';
+	char r = 'r';
+
+	putchar(n);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Description for function nested loops
