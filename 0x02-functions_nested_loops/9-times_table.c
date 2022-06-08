7f694e24cdf693cc5b411fa075fc45608a6aa022
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 18:52:59 -0700

    Added 9-times table script

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Function that prints the 9
+ * times table, starting with 0
+ *
+ *
+ * Return: Always success(0)
+ */
+
+void times_table(void)
+{
+	int a, b, result;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			result = (a * b);
+
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (result < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
