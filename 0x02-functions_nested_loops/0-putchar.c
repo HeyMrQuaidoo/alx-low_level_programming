82aef154fd6753f732d4fb6ad32f673e4fb3f9bd
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 17:42:37 -0700

    Added 0-putchar script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *  main - Prints _putchar, followed by new line
@@ -9,23 +9,15 @@
 
 int main(void)
 {
-	char n = '_';
-	char p = 'p';
-	char u = 'u';
-	char t = 't';
-	char c = 'c';
-	char h = 'h';
-	char a = 'a';
-	char r = 'r';
 
-	putchar(n);
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
-	putchar('\n');
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
