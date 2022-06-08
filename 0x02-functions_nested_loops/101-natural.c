476e02a3e8ee443f758c99b4b0a79701137d21fb
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 19:58:46 -0700

    Added 102 fibonacci script

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- *
- * main - Program that computes and prints 
- * the sum of all the multiples of 3 or 5 
+ * main - Program that computes and prints
+ * the sum of all the multiples of 3 or 5
  * below 1024 (excluded),
  *
  * Return: Always 0.
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Prints the first 50 Fibonacci numbers,
+  * starting with 1 and 2, followed by a new line.
+  *
+  * Return: Always success(0).
+  */
+int main(void)
+{
+	unsigned long count, a, b;
+
+	printf("%lu", bef);
+
+	for (count = 1; count < 50; count++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+	printf("\n");
+
+	return (0);
+}
