e3972ca3671e8d131c71dd618f40ca048072df60
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 18:59:33 -0700

    Added 10-add script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers
+ * a and b
+ * @a: First number to be added
+ * @b: Second number to be added
+ * Return: Always success(0)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
