9163548d63e73fd7663cd786f7a97f8e704565e2
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 20:42:25 -0700

    Added 104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,30 @@ int main(void)
 	unsigned long count;
 	unsigned long b = 1;
 	unsigned long a = 2;
+	unsigned long int i, c, d, e, f;
 
 	printf("%lu", b);
 
-	for (count = 1; count < 98; count++)
+	for (count = 1; count < 91; count++)
 	{
 		printf(", %lu", a);
 		a += b;
 		b = a - b;
+	}
+
+	c = (b / 1000000000);
+	d = (b % 1000000000);
+	e = (a / 1000000000);
+	f = (a % 1000000000);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", e + (f / 1000000000));
+		printf("%lu", f % 1000000000);
+		e = e + c;
+		c = e - c;
+		f = f + d;
+		d = f - d;
 	}
 	printf("\n");
 
