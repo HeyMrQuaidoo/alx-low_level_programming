306d0b0b0497685964970ece1649ec863ea3b946
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-07 20:26:01 -0700

    Added 104 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Prints the first 98 Fibonacci numbers,
+  * starting with 1 and 2, followed by a new line.
+  *
+  * Return: Always success(0).
+  */
+int main(void)
+{
+	unsigned long count;
+	unsigned long b = 1;
+	unsigned long a = 2;
+
+	printf("%lu", b);
+
+	for (count = 1; count < 98; count++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+	printf("\n");
+
+	return (0);
+}
