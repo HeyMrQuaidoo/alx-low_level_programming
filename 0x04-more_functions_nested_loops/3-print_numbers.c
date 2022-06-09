e50a47ed572f5150b2fc2b9cfcfd5cb985bc8ae4
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-10 00:02:27 +0100

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ *
+ * Return success(0)
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ *
+ * Return success(0)
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if ((n != 2) && (n != 4))
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+    return (0);
+}
