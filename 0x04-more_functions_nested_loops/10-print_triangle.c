4741c89ff37a0dcd74ba39a43f226b63aa569187
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-09 16:40:03 -0700

    more functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,13 @@ void print_triangle(int size)
 	for (row = 0; row < size; row++)
 	{
 		for (column = size - row; column > 1; column--)
+		{
 			_putchar(' ');
+		}
 		for (n = column + row; n >= 1; n--)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
