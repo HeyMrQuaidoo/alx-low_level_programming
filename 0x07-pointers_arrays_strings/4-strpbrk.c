6e052e7c6b7050ff5a7550bca9c5fb01d8b82c7c
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-20 18:12:49 -0700

    Update pointers and arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strpbrk -  function that searches a string for any of a set of bytes.
- * 
+ *
  * @s: pointer to string to be searched
  * @accept: pointer to character prefix to print
  * Return: Returns a pointer to the byte in s that matches one of
