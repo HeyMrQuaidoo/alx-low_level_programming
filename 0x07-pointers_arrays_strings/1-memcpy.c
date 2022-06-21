797f605fcedc5bb2f900344fbddc3b6f56c65f63
Code <codeninja94@gmail.com>
2022-06-21 00:27:30 +0000

    Update pointers and arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - ffunction that copies memory area
+ * @dest: dest pointer to be copied to
+ * @src: src pointer to be copied
+ * @n: number of byte to copy
+ * Return: s - pointer to memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	while (a < n)
+	{
+		dest[a] = src;
+		a++;
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function that locates a character in a string.
+ * @s: pointer to string to be searched
+ * @c: char to search for
+ * Return: Returns a pointer to the first occurrence
+ * of the character
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if ( *s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (!c ? s : NULL);
+}
