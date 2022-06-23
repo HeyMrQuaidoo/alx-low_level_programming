756fd32874afd054c5ea0c512860917f56af300e
Code <codeninja94@gmail.com>
2022-06-23 03:44:10 +0000

    Added recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+#include <stddef.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+#endif
