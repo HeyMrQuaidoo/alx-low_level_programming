62d9c7a952d2581a98208b62d6c4318eca8309df
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-22 20:55:09 -0700

    Update recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _putchar('\n');
-        _putchar(*s);
-    }
+	if (*s != '\0')
+	{
+		_putchar('\n');
+		_putchar(*s);
+	}
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,13 @@
  */
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return(-1);
-    }
-    if (n == 0 || n == 1)
-    {
-        return (1);
-    }
-    return (factorial(n - 1) * n);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (factorial(n - 1) * n);
 }
