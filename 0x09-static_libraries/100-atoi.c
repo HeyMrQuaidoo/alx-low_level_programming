d56b9634a79a32a18f458f8f29d82ff09e4a7674
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-24 16:35:58 -0700

    Added library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _atoi - converts string to integer
  * @s: pointer to string
@@ -6,5 +7,6 @@
  */
 int _atoi(char *s)
 {
+	printf("%d\n", s[0]);
 	return (0);
 }
