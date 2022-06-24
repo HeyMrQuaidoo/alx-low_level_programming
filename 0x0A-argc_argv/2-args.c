239a6cf57d6adfc5c9779ad91ef2e693224e082c
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-24 15:06:04 -0700

    Updated argc argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,16 @@
-Test
+#include <stdio.h>
+/**
+ * main -  program that prints all
+ * arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always success (0)
+ **/
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
