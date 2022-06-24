a5c992c0a14e5541224b8a593db684b56ba225aa
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-24 16:59:12 -0700

    Added library

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	change = atoi(argv[1]);
 	amt = 0;
 
@@ -48,4 +48,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", amt);
 	return (0);
 }
-
