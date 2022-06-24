19a7f065eb48d8f34248b0da74ad13b769cdf8e8
HeyMrQuaidoo <codeninja94@gmail.com>
2022-06-24 15:11:26 -0700

    Updated argc argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always success (0)
+ **/
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a + b);
+	return (0);
+}
