8b9d80c19cb86483cc00b8ca5e715053e18f75db
HeyMrQuaidoo <codeninja94@gmail.com>
2022-07-06 22:28:18 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+/**
+ * op_add - function to add two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of operands
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to substract two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ * Return: substraction of operands
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function multiplication of two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ * Return: mulitplication of operands
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function dividion os two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ * Return: division of operands
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Remainder of the division
+ *
+ * @a: first operand
+ * @b: second operand
+ * Return: remainder of operands
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
