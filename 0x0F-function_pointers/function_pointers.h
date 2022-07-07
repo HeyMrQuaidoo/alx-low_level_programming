02aaa6e8b725baa4114bc1feaeabe730c0820746
HeyMrQuaidoo <codeninja94@gmail.com>
2022-07-06 22:17:03 -0700

    function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef HEADERGUARD
-#define HEADERGUARD
+#ifndef HEADER_FILE
+#define HEADER_FILE function_pointers.h
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
