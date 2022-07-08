dd0186ec19fba2680605f4701e2ad3d9647261c8
HeyMrQuaidoo <codeninja94@gmail.com>
2022-07-07 22:10:18 -0700

    variadic functions:

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,8 +5,47 @@
  * @format: the format type to print
  * Return: Nothing
  */
-
 void print_all(const char * const format, ...)
 {
-	printf("%s", format);
+	unsigned int i = 0, j, curr = 0;
+	char *str;
+	const char format_arr[] = "cifs";
+	va_list vlist;
+
+	va_start(vlist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (format_arr[j])
+		{
+			if (format[i] == format_arr[j] && curr)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(vlist, int)), curr = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(vlist, int)), curr = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(vlist, double)), curr = 1;
+				break;
+			case 's':
+				str = va_arg(vlist, char *), curr = 1;
+				if (str)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+		} i++;
+	}
+	printf("\n");
+	va_end(vlist);
 }
