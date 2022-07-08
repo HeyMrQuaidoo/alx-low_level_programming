9ef742808d0cb1dc2965d2bbf21d4780177715ba
HeyMrQuaidoo <codeninja94@gmail.com>
2022-07-07 21:38:44 -0700

    variadic functions:

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - function that prints all anything.
+ * @format: the format type to print
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	printf("%s", format);
+}
