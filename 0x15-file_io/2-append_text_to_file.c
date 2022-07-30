455f1e52cb0c32c20ac3390ab439c4e8fbe5f88d
HeyMrQuaidoo <codeninja94@gmail.com>
2022-07-30 10:25:16 -0700

    file-io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,7 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == 0)
 		return (-1);
-	file_desc = open(filename, O_WRONLY | O_APPEND);
+	file_descr = open(filename, O_WRONLY | O_APPEND);
 	if (file_descr == -1)
 		return (-1);
 	if (text_content != 0)
